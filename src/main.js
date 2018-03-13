b9f8834d02363d81716a8dfaf2db04b6a9a0394b
feiqp <feiqingping@nnfe.com>
2018-03-13 10:27:11 +0800

    a

--- src/main.js
+++ src/main.js
@@ -10,11 +10,10 @@ import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import * as flexable from './assets/js/flexible'
 // import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/dist/css/swiper.min.css';
+// import 'swiper/dist/css/swiper.min.css';
 import {util} from '@/assets/js/util'
 import C from '@/assets/js/cookie'
 import $ from 'jquery';
-import 'babel-polyfill'
 
 
 Vue.config.productionTip = false
