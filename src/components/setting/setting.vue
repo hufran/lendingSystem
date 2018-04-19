e0da60c0be214d55cda843b28289ac05491539aa
hufuchen <hufuchen@nnfe.com>
2018-04-19 16:51:12 +0800

    bugxiugai

--- src/components/setting/setting.vue
+++ src/components/setting/setting.vue
@@ -58,7 +58,7 @@
         console.log(res)
         if(res.status ==0){
           let name=res.data.customerName;
-          that.customerName = name?name.substr(0,1)+name.substr(1,customerName.length-1).replace(/[\u4e00-\u9fa5]/g,'*'):window.userinfo.loginName.substring(0,3)+"****"+window.userinfo.loginName.substr(-4,4);
+          that.customerName = name?name.substr(0,1)+name.substr(1,customerName.length-1).replace(/[\u4e00-\u9fa5]/g,'*'):"";
           that.idCard = res.data.idCard
           that.mobile = res.data.mobile
           that.bankNo = res.data.LccbBankNo
